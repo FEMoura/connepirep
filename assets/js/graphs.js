ef3153e87b07f15871ec6ecafcddf7982488f9fe
principeluke <gabrielsantosifal@gmail.com>
2016-09-21 12:46:27 -0300

    Att. dos gráficos
    Atualização dos gráficos por área/ano e ano/área com dados reais.

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -7,7 +7,7 @@ $(function () {
             text: 'Quantidade de publicações por área'
         },
         xAxis: {
-            categories: ['Ciências Agrárias', 'Ciências Biológicas', 'Ciências Exatas e da Terra', 'Engenharias', 'Ciências Sociais e Aplicadas']
+            categories: ['Ciências Agrárias', 'Ciências Biológicas', 'Ciências Exatas e da Terra','Ciências Humanas','Ciências da Saúde','Ciências Sociais e Aplicadas','Engenharias', 'Linguística, Letras e Artes','Multidisciplinar']
         },
         yAxis: {
             min: 0,
@@ -51,13 +51,19 @@ $(function () {
         },
         series: [{
             name: '2006',
-            data: [5, 3, 4, 7, 2]
+            data: [9, 60, 43, 50, 7, 47, 60, 0, 0]
         }, {
             name: '2007',
-            data: [2, 2, 3, 2, 1]
+            data: [17, 21, 73, 15, 13, 54, 108, 1, 9]
         }, {
             name: '2008',
-            data: [3, 4, 4, 2, 5]
+            data: [0, 114, 130, 99, 0, 20, 55, 21, 0]
+        }, {
+            name: '2009',
+            data: [98, 66, 149, 131, 40, 115, 244, 28, 0]
+        }, {
+            name: '2010',
+            data: [140, 68, 150, 0, 226, 35, 163, 43, 204]
         }]
     });
 });
@@ -88,7 +94,192 @@ $(function () {
         },
         series: [{
             name: 'Total de publicações',
-            data: [276,311,440,1069,1029,1637,2195,2540]
+            data: [276,311,440,1044,1029,1637,2195,2540]
+        }]
+    });
+});
+
+$(function () {
+    $('#ano2').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Quantidade de publicações por área/ano'
+        },
+        xAxis: {
+            categories: ['2006', '2007', '2008', '2009', '2010']
+        },
+        yAxis: {
+            title: {
+                text: 'Quantidade'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Ciências Agrárias',
+            data: [9,17,0,98,140]
+        }, {
+            name: 'Ciências Biológicas',
+            data: [60, 21, 114, 66, 68]
+        }, {
+            name: 'Ciências Da Saúde',
+            data: [7, 13, 0, 40, 226]
+        }, {
+            name: 'Ciências Exatas e da Terra',
+            data: [43, 73, 130, 149, 150]
+        }, {
+            name: 'Ciências Humanas',
+            data: [50, 15, 199, 131, 0]
+        }, {
+            name: 'Ciências Sociais e Aplicadas',
+            data: [47, 54, 20, 115, 35]
+        }, {
+            name: 'Engenharias',
+            data: [60, 108, 55, 244, 163]
+        }, {
+            name: 'Linguística, Letras e Artes',
+            data: [0, 1, 21, 28, 43]
+        }, {
+            name: 'Multidisciplinar',
+            data: [0, 9, 0, 0, 204]
+        }]
+    });
+	
+	$(function () {
+    $('#ano3').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Quantidade de publicações por área/ano'
+        },
+        xAxis: {
+            categories: ['Ciências Agrárias', 'Ciências Biológicas', 'Ciências Exatas e da Terra','Ciências Humanas','Ciências da Saúde','Ciências Sociais e Aplicadas','Engenharias', 'Linguística, Letras e Artes','Multidisciplinar'],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Quantidade'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} publicações</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+       series: [{
+            name: '2006',
+            data: [9, 60, 43, 50, 7, 47, 60, 0, 0]
+        }, {
+            name: '2007',
+            data: [17, 21, 73, 15, 13, 54, 108, 1, 9]
+        }, {
+            name: '2008',
+            data: [0, 114, 130, 99, 0, 20, 55, 21, 0]
+        }, {
+            name: '2009',
+            data: [98, 66, 149, 131, 40, 115, 244, 28, 0]
+        }, {
+            name: '2010',
+            data: [140, 68, 150, 0, 226, 35, 163, 43, 204]
+        }]
+    });
+});
+});
+
+$(function () {
+    $('#ano4').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Quantidade de publicações por ano/área'
+        },
+        xAxis: {
+            categories: ['2006', '2007', '2008', '2009', '2010'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Quantidade',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' publicações'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Ciências Agrárias',
+            data: [9,17,0,98,140]
+        }, {
+            name: 'Ciências Biológicas',
+            data: [60, 21, 114, 66, 68]
+        }, {
+            name: 'Ciências Da Saúde',
+            data: [7, 13, 0, 40, 226]
+        }, {
+            name: 'Ciências Exatas e da Terra',
+            data: [43, 73, 130, 149, 150]
+        }, {
+            name: 'Ciências Humanas',
+            data: [50, 15, 199, 131, 0]
+        }, {
+            name: 'Ciências Sociais e Aplicadas',
+            data: [47, 54, 20, 115, 35]
+        }, {
+            name: 'Engenharias',
+            data: [60, 108, 55, 244, 163]
+        }, {
+            name: 'Linguística, Letras e Artes',
+            data: [0, 1, 21, 28, 43]
+        }, {
+            name: 'Multidisciplinar',
+            data: [0, 9, 0, 0, 204]
         }]
     });
 });
--- painel.php
+++ painel.php
@@ -61,140 +61,19 @@
     <p class="ls-float-right ls-float-none-xs ls-small-info"><strong></strong></p>
     <h2 class="ls-title-3">Gráficos</h2>
   </header>
-  <div id="area" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+  <div id="ano" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
  
  </div>
  
  <div class="ls-box ls-board-box">
-  <div id="ano" style="min-width:620px; height: 400px; margin: 0 auto"></div>
+  <div id="ano3" style="min-width:310px; height: 400px; margin: 0 auto"></div>
  </div>
-  
-<div class="ls-box ls-board-box">
-  <div id="sending-stats" class="row ls-clearfix">
-    <div class="col-sm-12 col-md-6">
-      <div class="ls-box">
-        <div class="ls-box-head">
-          <h6 class="ls-title-4">Quantidade de publicações por área</h6>
-        </div>
-        <div class="ls-box-body">
-          <div class="ct-chart ct-perfect-fourth ct-golden-section" id="chart2"></div>
-        </div>
-      </div>
-    </div>
+ 
+ <div class="ls-box ls-board-box">
+  <div id="ano4" style="min-width:310px; height: 800px; margin:0 auto"></div>
  </div>
-</div>
-
-<div class="ls-box ls-board-box">
-  <div id="sending-stats" class="row ls-clearfix">
-    <div class="col-sm-12 col-md-12">
-      <div class="ls-box">
-        <div class="ls-box-head">
-          <h6 class="ls-title-4">Quantidade de publicações por ano</h6>
-        </div>
-        <div class="ls-box-body">
-          <div class="ct-chart ct-perfect-fourth ct-golden-section" id="chart5"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<div class="ls-box ls-board-box">
-  <div id="sending-stats" class="row ls-clearfix">
-    <div class="col-sm-12 col-md-12">
-      <div class="ls-box">
-        <div class="ls-box-head">
-          <h6 class="ls-title-4">Quantidade de publicações por IF</h6>
-        </div>
-        <div class="ls-box-body">
-          <div class="ct-chart ct-perfect-fourth ct-golden-section" id="chart7"></div>
-        </div>
-      </div>
-    </div>
- </div>
-</div>
+ 
 <!-- ---------------- -->
-
-<?php 
-// Quantidade de publicações por área
-
-$qpa = new Read();
-$qpa->FullRead("SELECT count(*), area FROM publicacao WHERE aprovado='S' GROUP BY area");
-
-$qtd_qpa = array();
-$area_qpa = array();
-
-foreach ($qpa->getResult() as $q){
-	$qtd_qpa[] = $q['count(*)'];
-	$area_qpa[] = $q['area'];
-}
-//tranforma em string
-$qtd_qpa = implode(', ', $qtd_qpa);
-
-//transforma em string
-$area_qpa = '"'.implode('", "', $area_qpa).'"';
-
-// Quantidade de publicações por ano
-
-$qpano = new Read();
-$qpano->FullRead("SELECT count(*), ano FROM publicacao WHERE aprovado='S' GROUP BY ano");
-
-$countAno = array();
-$ano = array();
-
-foreach ($qpano->getResult() as $a){
-	$countAno[] = $a['count(*)'];
-	$ano[] = $a['ano'];
-}
-//tranforma em string
-$countAno = implode(', ', $countAno);
-
-//transforma em string
-$ano = '"'.implode('", "', $ano).'"';
-
-// Quantidade de publicação por IF
-
-$qpi = new Read();
-$qpi->FullRead("SELECT count(*), ies FROM publicacao WHERE aprovado='S' GROUP BY ies");
-
-$qtd_qpi = array();
-$ies_qpi = array();
-
-foreach ($qpi->getResult() as $q){
-	$qtd_qpi[] = $q['count(*)'];
-	$ies_qpi[] = $q['ies'];
-}
-//tranforma em string
-$qtd_qpi = implode(', ', $qtd_qpi);
-//transforma em string
-$ies_qpi = '"'.implode('", "', $ies_qpi).'"';
-?>
-
-<script>
-  // Gráfico de barra com a quantidade de artigos por área.
-  new Chartist.Bar('#chart2', {
-    labels: [<?php echo $area_qpa; ?>],
-    series: [[<?php echo $qtd_qpa; ?>]]
-  });
-
-  // Gráfico de linha com a quantidade de artigos por ano.
-   new Chartist.Line('#chart5', {
-	   labels: [<?php echo $ano; ?>],
-	   series: [[<?php echo $countAno; ?>]]
-	 }, {
-	   low: 0,
-	   showArea: true
-	 });
-
-  // Gráfico de barra com a quantidade de artigos por IF
-  new Chartist.Bar('#chart7', {
-    labels: [<?php echo $ies_qpi; ?>],
-    series: [[<?php echo $qtd_qpi; ?>]]
-  });
-</script>
-
-
     </div>
       <?php require_once('footer.php');?>
     </main>
